35f2954408d4a5a92aefbcce5ec98672c2e02761
Shanky <varundeepanshu@gmail.com>
2022-05-15 22:52:27 +0530

    backend

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const URI = 'mongodb+srv://hritik:123@freecluster.51vdj.mongodb.net/OxusMotors?retryWrites=true&w=majority'
+const app = express()
+
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('db connected..!');
+};
+
+connectDB()
+
+app.use(express.json())
+
+const carRouter = require('./routes/cars')
+app.use('/cars', carRouter)
+
+const inventoryRouter = require('./routes/inventory')
+app.use('/inventory', inventoryRouter)
+
+const contactUsRouter = require('./routes/contactUs')
+app.use('/contactUs', contactUsRouter)
+
+const homeRouter = require('./routes/home')
+app.use('/home', homeRouter)
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, () => {
+  console.log('Server started')
+})
